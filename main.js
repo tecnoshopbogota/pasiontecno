7b7758257b14b9efe7c504457b757e7d81a56994
tecnoshopbogota <168027388+tecnoshopbogota@users.noreply.github.com>
2024-04-30 23:28:37 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let products = document.querySelectorAll('.product');
-
-products.forEach(product => {
-    product.addEventListener('click', function() {
-        this.classList.toggle('expanded');
-    });
-});
