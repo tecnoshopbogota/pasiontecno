a2fa2e1a5fb8ecde773c892f17a14b9af8c606f8
tecnoshopbogota <168027388+tecnoshopbogota@users.noreply.github.com>
2024-05-03 12:32:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-let products = document.querySelectorAll('.product');
+let products = document.querySelectorAll('.k-product-contenedor');
 
-products.forEach(product => {
+products.forEach((product, index) => {
     product.addEventListener('click', function() {
-        // Primero, elimina la clase 'expanded' de todos los productos
+        // Primero, resetea el 'grid-row' de todos los productos
         products.forEach(p => {
-            if (p !== this) {
-                p.classList.remove('expanded');
-            }
+            p.style.gridRow = 'auto / span 1';
         });
 
         // Luego, si el producto en el que se hizo clic ya tiene la clase 'expanded', la elimina; si no la tiene, la agrega
         this.classList.toggle('expanded');
+
+        // Si el producto en el que se hizo clic tiene la clase 'expanded', cambia su 'grid-row' para que se muestre primero y ocupe todas las filas restantes
+        if (this.classList.contains('expanded')) {
+            this.style.gridRow = '1 / span 100';
+        }
     });
 });
-
 let precios = document.querySelectorAll('.product-price');
 
 precios.forEach(precio => {
