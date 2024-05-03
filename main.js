3d2034b9f9eb3412553b355d6cf1ae9075ff5135
tecnoshopbogota <168027388+tecnoshopbogota@users.noreply.github.com>
2024-05-03 13:51:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let products = document.querySelectorAll('.product');
+let scrollPosition = 0; // Variable para guardar la posición de desplazamiento
 
 products.forEach(product => {
-    product.addEventListener('click', function() {
+    product.addEventListener('click', function(event) {
+        // Guardar la posición de desplazamiento antes de expandir el producto
+        scrollPosition = window.scrollY;
+
         // Primero, elimina la clase 'expanded' de todos los productos
         products.forEach(p => {
             if (p !== this) {
@@ -11,6 +15,13 @@ products.forEach(product => {
 
         // Luego, si el producto en el que se hizo clic ya tiene la clase 'expanded', la elimina; si no la tiene, la agrega
         this.classList.toggle('expanded');
+
+        // Restaurar la posición de desplazamiento después de expandir o contraer el producto
+        if (this.classList.contains('expanded')) {
+            // Si se está expandiendo el producto, evitar que la página se mueva
+            event.preventDefault();
+            window.scrollTo(0, scrollPosition);
+        }
     });
 });
 
