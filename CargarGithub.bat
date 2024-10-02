797917062fa7826f01936e0366f542b9ee90955b
Tu Nombre <tu_correo@example.com>
2024-10-01 21:17:38 -0500

    actualizacion

--- CargarGithub.bat
+++ CargarGithub.bat
@@ -1,7 +1,11 @@
+@echo off
+cd "C:\Users\cod_d\OneDrive\Documentos\TECNO SHOP\Pagina WEB\pasiontecno"
+
+git config --global user.email "tu_correo@example.com"
+git config --global user.name "Tu Nombre"
+
 git add .
-git commit -m "Carga de archivo"
+git commit -m "actualizacion"
 git pull origin main
 git push origin main
-
 pause
-
